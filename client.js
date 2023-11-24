880953452ca00368cf52712ec54384efd598833b
pektrass <98953977+pektrass@users.noreply.github.com>
2023-11-24 10:41:41 +0300

    Add files via upload

---
+++ client.js
@@ -0,0 +1,29 @@
+
+document.getElementById('but').addEventListener('click', async function (event) {
+    event.preventDefault();
+
+    const reviewInput = document.getElementById('input');
+    const reviewText = reviewInput.value;
+
+    // Отправка отзыва на сервер
+    const response = await fetch('http://localhost:3001/saveReview', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ comment: reviewText }),
+    });
+
+    if (response.ok) {
+        console.log('Отзыв успешно сохранен');
+        // По желанию, обновите пользовательский интерфейс или очистите поле ввода
+        reviewInput.value = '';
+    } else {
+        console.error('Ошибка при сохранении отзыва:', response.statusText);
+    }
+});
+
+function clearInput()
+{
+  document.getElementById('input').value = ''; // Очистить поле ввода
+}
